04eecd7852894f6571fbb843f37d2bac679177c3
Johan van den Berg <johantheitguy@gmail.com>
2019-05-03 07:18:45 +0200

    No lazy loading modules. All just one big module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,36 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+import { LoginPage } from './login/login.page';
+import { AboutPage } from './home/about/about.page';
+import { SettingsPage } from './home/settings/settings.page';
+import { UsernamePage } from './login/username/username.page';
+import { PasswordPage } from './login/password/password.page';
+import { IonicModule } from '@ionic/angular';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+  { path: 'home', component: HomePage,
+    children: [
+      { path: 'about', component: AboutPage },
+      { path: 'settings', component: SettingsPage }
+    ]
+  },
+  { path: 'login', component: LoginPage,
+    children: [
+      { path: 'username', component: UsernamePage },
+      { path: 'password', component: PasswordPage }
+    ]
+  }
 ];
 
 @NgModule({
   imports: [
+    IonicModule,
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  declarations: [
+    HomePage, AboutPage, SettingsPage, LoginPage, UsernamePage, PasswordPage
   ],
   exports: [RouterModule]
 })
